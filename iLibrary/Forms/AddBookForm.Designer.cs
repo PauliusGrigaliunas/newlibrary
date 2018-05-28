df01e652f200624baf1bdee25837902bda0e7845
Moduxx <31692532+Moduxx@users.noreply.github.com>
2018-05-28 19:29:37 +0300

    Design added
    some forms added

--- iLibrary/Forms/AddBookForm.Designer.cs
+++ iLibrary/Forms/AddBookForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace iLibrary
+﻿namespace iLibrary.Forms
 {
-    partial class userForm
+    partial class AddBookForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AddBookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "userForm";
+            this.ClientSize = new System.Drawing.Size(459, 290);
+            this.Name = "AddBookForm";
+            this.Text = "Pridėti knygą";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ iLibrary/Forms/UserSettingsForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace iLibrary.Forms
+{
+    partial class UserSettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.greetLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // greetLabel
+            // 
+            this.greetLabel.AutoSize = true;
+            this.greetLabel.Location = new System.Drawing.Point(195, 24);
+            this.greetLabel.Name = "greetLabel";
+            this.greetLabel.Size = new System.Drawing.Size(140, 13);
+            this.greetLabel.TabIndex = 0;
+            this.greetLabel.Text = "Sveiki prisjungę (UserName)";
+            // 
+            // UserSettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 355);
+            this.Controls.Add(this.greetLabel);
+            this.Name = "UserSettingsForm";
+            this.Text = "Vartotojo meniu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label greetLabel;
+    }
+}
