ff279c5a67b0d561e891d139b0f9513acfe8a94d
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2018-05-29 09:26:46 +0300

    save

--- iLibrary/Forms/EmployeeForm.Designer.cs
+++ iLibrary/Forms/EmployeeForm.Designer.cs
@@ -29,27 +29,13 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmployeeForm));
             this.workerLabel = new System.Windows.Forms.Label();
             this.addBook = new System.Windows.Forms.Button();
             this.removeBookButton = new System.Windows.Forms.Button();
             this.addUnitButton = new System.Windows.Forms.Button();
             this.deleteUnitButton = new System.Windows.Forms.Button();
-            this.knygosBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
             this.knygosBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.libraryDataDataSet = new iLibrary.LibraryDataDataSet();
-            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.knygosBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
             this.knygosTableAdapter = new iLibrary.LibraryDataDataSetTableAdapters.KnygosTableAdapter();
             this.tableAdapterManager = new iLibrary.LibraryDataDataSetTableAdapters.TableAdapterManager();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -58,8 +44,6 @@
             this.Metai = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Skaitytojas = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.bwcListView = new System.Windows.Forms.ListView();
-            ((System.ComponentModel.ISupportInitialize)(this.knygosBindingNavigator)).BeginInit();
-            this.knygosBindingNavigator.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.knygosBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.libraryDataDataSet)).BeginInit();
             this.SuspendLayout();
@@ -115,47 +99,6 @@
             this.deleteUnitButton.Text = "Pašalinti egzempliorių";
             this.deleteUnitButton.UseVisualStyleBackColor = true;
             // 
-            // knygosBindingNavigator
-            // 
-            this.knygosBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
-            this.knygosBindingNavigator.BindingSource = this.knygosBindingSource;
-            this.knygosBindingNavigator.CountItem = this.bindingNavigatorCountItem;
-            this.knygosBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
-            this.knygosBindingNavigator.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.knygosBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.bindingNavigatorMoveFirstItem,
-            this.bindingNavigatorMovePreviousItem,
-            this.bindingNavigatorSeparator,
-            this.bindingNavigatorPositionItem,
-            this.bindingNavigatorCountItem,
-            this.bindingNavigatorSeparator1,
-            this.bindingNavigatorMoveNextItem,
-            this.bindingNavigatorMoveLastItem,
-            this.bindingNavigatorSeparator2,
-            this.bindingNavigatorAddNewItem,
-            this.bindingNavigatorDeleteItem,
-            this.knygosBindingNavigatorSaveItem});
-            this.knygosBindingNavigator.Location = new System.Drawing.Point(0, 0);
-            this.knygosBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
-            this.knygosBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
-            this.knygosBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
-            this.knygosBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
-            this.knygosBindingNavigator.Name = "knygosBindingNavigator";
-            this.knygosBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
-            this.knygosBindingNavigator.Size = new System.Drawing.Size(644, 27);
-            this.knygosBindingNavigator.TabIndex = 3;
-            this.knygosBindingNavigator.Text = "bindingNavigator1";
-            this.knygosBindingNavigator.RefreshItems += new System.EventHandler(this.knygosBindingNavigator_RefreshItems);
-            // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(24, 24);
-            this.bindingNavigatorAddNewItem.Text = "Add new";
-            // 
             // knygosBindingSource
             // 
             this.knygosBindingSource.DataMember = "Knygos";
@@ -165,91 +108,6 @@
             // 
             this.libraryDataDataSet.DataSetName = "LibraryDataDataSet";
             this.libraryDataDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // bindingNavigatorCountItem
-            // 
-            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(45, 24);
-            this.bindingNavigatorCountItem.Text = "of {0}";
-            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(24, 24);
-            this.bindingNavigatorDeleteItem.Text = "Delete";
-            // 
-            // bindingNavigatorMoveFirstItem
-            // 
-            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
-            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
-            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(24, 24);
-            this.bindingNavigatorMoveFirstItem.Text = "Move first";
-            // 
-            // bindingNavigatorMovePreviousItem
-            // 
-            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
-            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
-            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(24, 24);
-            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
-            // 
-            // bindingNavigatorSeparator
-            // 
-            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 27);
-            // 
-            // bindingNavigatorPositionItem
-            // 
-            this.bindingNavigatorPositionItem.AccessibleName = "Position";
-            this.bindingNavigatorPositionItem.AutoSize = false;
-            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
-            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 27);
-            this.bindingNavigatorPositionItem.Text = "0";
-            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
-            // 
-            // bindingNavigatorSeparator1
-            // 
-            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
-            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 27);
-            // 
-            // bindingNavigatorMoveNextItem
-            // 
-            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
-            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
-            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(24, 24);
-            this.bindingNavigatorMoveNextItem.Text = "Move next";
-            // 
-            // bindingNavigatorMoveLastItem
-            // 
-            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
-            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
-            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(24, 24);
-            this.bindingNavigatorMoveLastItem.Text = "Move last";
-            // 
-            // bindingNavigatorSeparator2
-            // 
-            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
-            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 27);
-            // 
-            // knygosBindingNavigatorSaveItem
-            // 
-            this.knygosBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.knygosBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("knygosBindingNavigatorSaveItem.Image")));
-            this.knygosBindingNavigatorSaveItem.Name = "knygosBindingNavigatorSaveItem";
-            this.knygosBindingNavigatorSaveItem.Size = new System.Drawing.Size(24, 24);
-            this.knygosBindingNavigatorSaveItem.Text = "Save Data";
-            this.knygosBindingNavigatorSaveItem.Click += new System.EventHandler(this.knygosBindingNavigatorSaveItem_Click);
             // 
             // knygosTableAdapter
             // 
@@ -305,7 +163,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(644, 574);
             this.Controls.Add(this.bwcListView);
-            this.Controls.Add(this.knygosBindingNavigator);
             this.Controls.Add(this.deleteUnitButton);
             this.Controls.Add(this.addUnitButton);
             this.Controls.Add(this.removeBookButton);
@@ -314,9 +171,6 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "EmployeeForm";
             this.Text = "Darbuotojo meniu";
-            ((System.ComponentModel.ISupportInitialize)(this.knygosBindingNavigator)).EndInit();
-            this.knygosBindingNavigator.ResumeLayout(false);
-            this.knygosBindingNavigator.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.knygosBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.libraryDataDataSet)).EndInit();
             this.ResumeLayout(false);
@@ -335,19 +189,6 @@
         private System.Windows.Forms.BindingSource knygosBindingSource;
         private LibraryDataDataSetTableAdapters.KnygosTableAdapter knygosTableAdapter;
         private LibraryDataDataSetTableAdapters.TableAdapterManager tableAdapterManager;
-        private System.Windows.Forms.BindingNavigator knygosBindingNavigator;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
-        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
-        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
-        private System.Windows.Forms.ToolStripButton knygosBindingNavigatorSaveItem;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Leidykla;
