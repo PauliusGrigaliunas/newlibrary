7779789df8ee5a741176f79d55ad9fa519547160
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2018-05-29 09:06:01 +0300

    Add Book class

---
+++ iLibrary/Logic/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iLibrary.Logic
+{
+    public class Book
+    {
+        public int Isbn { get; set; }
+        public string Pavadinimas { get; set; }
+        public string Autorius { get; set; }
+        public string Leidykla { get; set; }
+        public DateTime? Metai { get; set; }
+    }
+}
